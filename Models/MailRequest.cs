701f234cdcf97664b17316c7762f5daea85c7841
Siripatel Chagarla <siripatel.chagarla@motivitylabs.com>
2023-03-21 11:43:14 +0530

    SendEmail

---
+++ Models/MailRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json.Serialization;
+
+namespace ECommerce_ShoppingCart_Common.Models
+{
+    public class MailRequest
+    {
+        public int Id { get; set; }
+        public string? ToEmail { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+        [JsonIgnore]
+        public bool? IsActive { get; set; }
+        public List<IFormFile> Attachments { get; set; }
+    }
+}
